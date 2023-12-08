4639c75d30d128246bd32995e97c699050ffe503
vonsogwapo <von.manginsay71@gmail.com>
2023-12-09 00:00:21 +0800

    product display and still making dashboard

--- client/src/Pages/StaffPages/Dashboard.jsx
+++ client/src/Pages/StaffPages/Dashboard.jsx
@@ -3,7 +3,21 @@ import "../../index.css";
 function Dashboard() {
   return (
     <>
-      <h1>This is Dashboard.jsx</h1>
+       <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-3xl font-semibold mb-4">Still making</h1>
+        <p className="text-gray-700 mb-8">
+          description
+        </p>
+        <div className="flex justify-center">
+          <img
+            src="" 
+            alt="Banana Cupcakes"
+            className="rounded-md"
+          />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
