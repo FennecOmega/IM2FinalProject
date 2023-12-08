d09a11dcacb19ecf203386053d5c3ed818c7b62d
Fennec Omega <philipisidrogo@gmail.com>
2023-12-08 12:43:42 +0800

    Added useCartContext

--- client/src/Components/DashboardSideBar.jsx
+++ client/src/Components/DashboardSideBar.jsx
@@ -8,129 +8,202 @@ const DashboardSideBar = ({ isOpen, onClose }) => {
   const sidebarStyles = {
     position: "absolute",
     top: "1",
-    left: isOpen ? 0 : "-150px", 
-    width: "64px", 
-    backgroundColor: "#fff", 
+    left: isOpen ? 0 : "-150px",
+    width: "64px",
+    backgroundColor: "#fff",
     transition: "left 0.3s ease-in-out",
   };
   return (
     <div style={sidebarStyles}>
-      <div class="flex flex-row">
-  <aside>
-    <div class="flex w-[150px] flex-col shadow-xl">
-      <nav class="flex-1 divide-y-2">
-        <ul class="flex flex-col gap-y-2">
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-hand" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M18 9l-6 -6l-9 9h2v7a2 2 0 0 0 2 2h3.5" />
-                  <path d="M9 21v-6a2 2 0 0 1 2 -2h2" />
-                  <path d="M16 17.5l-.585 -.578a1.516 1.516 0 0 0 -2 0c-.477 .433 -.551 1.112 -.177 1.622l1.762 2.456c.37 .506 1.331 1 2 1h3c1.009 0 1.497 -.683 1.622 -1.593c.252 -.938 .378 -1.74 .378 -2.407c0 -1 -.939 -1.843 -2 -2h-1v-2.636c0 -.754 -.672 -1.364 -1.5 -1.364s-1.5 .61 -1.5 1.364v4.136z" />
-                </svg>
-                Dashboard
-              </div>
-            </a>
-          </li>
+      <div className="flex flex-row">
+        <aside>
+          <div className="flex w-[150px] flex-col shadow-xl">
+            <nav className="flex-1 divide-y-2">
+              <ul className="flex flex-col gap-y-2">
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-home-hand"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M18 9l-6 -6l-9 9h2v7a2 2 0 0 0 2 2h3.5" />
+                        <path d="M9 21v-6a2 2 0 0 1 2 -2h2" />
+                        <path d="M16 17.5l-.585 -.578a1.516 1.516 0 0 0 -2 0c-.477 .433 -.551 1.112 -.177 1.622l1.762 2.456c.37 .506 1.331 1 2 1h3c1.009 0 1.497 -.683 1.622 -1.593c.252 -.938 .378 -1.74 .378 -2.407c0 -1 -.939 -1.843 -2 -2h-1v-2.636c0 -.754 -.672 -1.364 -1.5 -1.364s-1.5 .61 -1.5 1.364v4.136z" />
+                      </svg>
+                      Dashboard
+                    </div>
+                  </a>
+                </li>
 
-          {/* The list are still tentatives, these are just sample li i've put  */}
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-assembly" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z" />
-                  <path d="M15.5 9.422c.312 .18 .503 .515 .5 .876v3.277c0 .364 -.197 .7 -.515 .877l-3 1.922a1 1 0 0 1 -.97 0l-3 -1.922a1 1 0 0 1 -.515 -.876v-3.278c0 -.364 .197 -.7 .514 -.877l3 -1.79c.311 -.174 .69 -.174 1 0l3 1.79h-.014z" />
-                </svg>
-                Products
-              </div>
-            </a>
-          </li>
+                {/* The list are still tentatives, these are just sample li i've put  */}
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-assembly"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z" />
+                        <path d="M15.5 9.422c.312 .18 .503 .515 .5 .876v3.277c0 .364 -.197 .7 -.515 .877l-3 1.922a1 1 0 0 1 -.97 0l-3 -1.922a1 1 0 0 1 -.515 -.876v-3.278c0 -.364 .197 -.7 .514 -.877l3 -1.79c.311 -.174 .69 -.174 1 0l3 1.79h-.014z" />
+                      </svg>
+                      Products
+                    </div>
+                  </a>
+                </li>
 
-        
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-truck-delivery" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                  <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                  <path d="M5 17h-2v-4m-1 -8h11v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" />
-                  <path d="M3 9l4 0" />
-                </svg>
-                Orders
-              </div>
-            </a>
-          </li>
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-truck-delivery"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                        <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                        <path d="M5 17h-2v-4m-1 -8h11v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" />
+                        <path d="M3 9l4 0" />
+                      </svg>
+                      Orders
+                    </div>
+                  </a>
+                </li>
 
-         
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-report-analytics" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-                  <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-                  <path d="M9 17v-5" />
-                  <path d="M12 17v-1" />
-                  <path d="M15 17v-3" />
-                </svg>
-                Sales
-              </div>
-            </a>
-          </li>
-         
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-                  <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                  <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                  <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                </svg>
-                Users
-              </div>
-            </a>
-          </li>
-        </ul>
-        <ul>
-        
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-square-rounded" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 13a3 3 0 1 0 0 -6a3 3 0 0 0 0 6z" />
-                  <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
-                  <path d="M6 20.05v-.05a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v.05" />
-                </svg>
-                Profile
-              </div>
-            </a>
-          </li>
-       
-          <li>
-            <a href="/">
-              <div class="flex cursor-pointer items-center gap-2 px-3 py-3 hover:bg-green-700 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-                  <path d="M9 12h12l-3 -3" />
-                  <path d="M18 15l3 -3" />
-                </svg>
-                Logout
-              </div>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </aside>
-</div>
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-report-analytics"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+                        <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+                        <path d="M9 17v-5" />
+                        <path d="M12 17v-1" />
+                        <path d="M15 17v-3" />
+                      </svg>
+                      Sales
+                    </div>
+                  </a>
+                </li>
 
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-users"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                        <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                      </svg>
+                      Users
+                    </div>
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-user-square-rounded"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 13a3 3 0 1 0 0 -6a3 3 0 0 0 0 6z" />
+                        <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+                        <path d="M6 20.05v-.05a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v.05" />
+                      </svg>
+                      Profile
+                    </div>
+                  </a>
+                </li>
+
+                <li>
+                  <a href="/">
+                    <div className="flex items-center gap-2 px-3 py-3 cursor-pointer hover:bg-green-700 hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-logout"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                        <path d="M9 12h12l-3 -3" />
+                        <path d="M18 15l3 -3" />
+                      </svg>
+                      Logout
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </aside>
+      </div>
     </div>
   );
 };
