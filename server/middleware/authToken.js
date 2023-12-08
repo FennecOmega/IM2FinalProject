feab044985afd34ede5cf800aa55586f1ecf04d7
K&K <78479612+KKYuuki@users.noreply.github.com>
2023-12-09 01:12:00 +0800

    Update authToken.js

--- server/middleware/authToken.js
+++ server/middleware/authToken.js
@@ -1,17 +1,16 @@
-const { response } = require('express');
 const jwt = require('jsonwebtoken');
 
 const authToken = (req,res,next) => {
-    const authHeader = req.headers["authorized"];
-    const token = authHeader && authHeader.split(" ")[1];
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];    
 
     if(!token){
-        return response.status(403).json({ message: "Unauthorized access"});
+        return res.status(403).json({ message: "Unauthorized access"});
     }
 
     jwt.verify(token, "secretToken", (error, user) => {
         if(error){
-            return response.status(403).json({ message: "Invalid"});
+            return res.status(403).json({ message: "Invalid"});
         }
 
         req.user = user;
